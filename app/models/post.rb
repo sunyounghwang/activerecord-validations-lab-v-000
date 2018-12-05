bde8cf4e8d98d9adfae78f9a62367681f1424160
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-12-05 18:04:32 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait
+
+  def clickbait
+    if title
+      unless ["Won't Believe", "Secret", "Top [number]", "Guess"].any? { |phrase| title.include?(phrase) }
+        errors.add(:title, "must contain 'Won't Believe', 'Secret', 'Top [number]', or 'Guess'")
+      end
+    end
+  end
 end
